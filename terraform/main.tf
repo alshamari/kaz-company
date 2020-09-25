3a6d1ffcee86702462add1df8454b1bd19c95614
ahmed alshammari <ahmed.aalshamari@gmail.com>
2020-09-25 19:02:37 -0400

    pushing terraform

---
+++ terraform/main.tf
@@ -0,0 +1,43 @@
+provider "google" {
+  credentials = file("centering-force-290618-8ea206f7845c.json")
+  project     = "centering-force-290618"
+  region      = "us-central1"
+}
+
+
+resource "google_compute_instance" "default" {
+  name         = "myapp"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  tags = ["http", "https"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    env = "dev"
+  }
+
+  metadata_startup_script = "curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python get-pip.py --user && sudo python -m pip install ansible"
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
